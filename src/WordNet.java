d26db2ae5e5bc21526890f1678a6f69933822c21
Koen de Roo <roo.k@tkppensioen.nl>
2018-01-19 12:33:48 +0100

    changed datatypes and added private readers for hypernyms and synsets

--- src/WordNet.java
+++ src/WordNet.java
@@ -1,47 +1,34 @@
-import edu.princeton.cs.algs4.*;
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.DirectedCycle;
+import edu.princeton.cs.algs4.In;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WordNet {
 
     //datastructures
-    private final ST<String, Integer> allNouns;
-    private final ST<Integer, String> allKeys;
+    private Map<String, ArrayList<Integer>> allNouns = new HashMap<>();
+    private Map<Integer, String> allKeys = new HashMap<>();
+    private Map<Integer, ArrayList<Integer>> edges = new HashMap<>(); // Edges between vertices
     private SAP sap;
     private Digraph digraph;
+    private int V = 0;
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
-        In in = new In(hypernyms);
-        // create empty Digraph
-        String[] allHypernyms = in.readAllLines();
-        digraph = new Digraph(allHypernyms.length);
-        // fill it up
-        for(String h: allHypernyms) {
-            String[] parts = h.split(",");
-            // only add when exists of 2 parts (last line is not)
-            if (parts.length < 2) break;
-            // add new Bag when not exist
-            Integer v = Integer.parseInt(parts[0]);
-            digraph.addEdge(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
+        loadSynsets(synsets);
+        loadHypernyms(hypernyms);
 
+        digraph = new Digraph(V);
+        // fill it up
+        for (Map.Entry<Integer, ArrayList<Integer>> entry : edges.entrySet()) {
+            for (Integer w : entry.getValue()) {
+                this.digraph.addEdge(entry.getKey(), w);
+            }
         }
 
-        sap = new SAP(digraph);
-
-        in = new In(synsets);
-        // create list of nouns
-        allNouns = new ST<String, Integer>();
-        allKeys = new ST<>();
-        // fill it up
-        while (in.hasNextLine()) {
-            String[] parts = in.readLine().split(",");
-            // only add when exists of 2 parts (last line is not)
-            if (parts.length < 2) break;
-
-            allNouns.put(parts[1], Integer.parseInt(parts[0]));
-            allKeys.put(Integer.parseInt(parts[0]), parts[1]);
-        }
         // check cycle
         DirectedCycle finder = new DirectedCycle(digraph);
         if (finder.hasCycle()) {
@@ -49,21 +36,20 @@ public class WordNet {
         }
 
         // Find all roots
-        SET<Integer> roots = new SET<Integer>();
-        for (int i = 0; i < digraph.V(); i++) {
-            DirectedDFS dfs = new DirectedDFS(digraph, i);
-            if (dfs.count() == 1) {
-                roots.add(i);
-            }
+        int roots = 0;
+        for (int i =0; i < digraph.V(); i++) {
+            if (!digraph.adj(i).iterator().hasNext()) roots++;
         }
-        if (roots.size() > 1) {
+        if (roots != 1) {
             throw new IllegalArgumentException();
         }
+
+        sap = new SAP(digraph);
     }
 
     // returns all WordNet nouns
     public Iterable<String> nouns() {
-        return allNouns.keys();
+        return this.allNouns.keySet();
     }
 
     // is the word a WordNet noun? (aka is the word in list of nouns
@@ -71,7 +57,7 @@ public class WordNet {
         if (word == null) {
             throw new NullPointerException();
         }
-        return allNouns.contains(word);
+        return this.allNouns.containsKey(word);
     }
 
     // distance between nounA and nounB (defined below)
@@ -80,11 +66,7 @@ public class WordNet {
             throw new IllegalArgumentException();
         }
 
-        // get index of nouns
-        Integer idA = allNouns.get(nounA);
-        Integer idB = allNouns.get(nounB);
-
-        return sap.length(idA, idB);
+        return sap.length(allNouns.get(nounA), allNouns.get(nounB));
     }
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
@@ -94,19 +76,78 @@ public class WordNet {
             throw new IllegalArgumentException();
         }
 
-        // get index of nouns
-        Integer idA = allNouns.get(nounA);
-        Integer idB = allNouns.get(nounB);
-
-        Integer ancestor = sap.ancestor(idA, idB);
+        Integer ancestor = sap.ancestor(allNouns.get(nounA), allNouns.get(nounB));
         return allKeys.get(ancestor);
+    }
+
+    // load synsets into two lists (one for lookup, one for index)
+    private void loadSynsets(String synsets) {
+        In in = new In(synsets);
+        String line;
+
+        ArrayList<Integer> currentNounsList;
+        String currentSynsetNouns;
+
+        while ((line = in.readLine()) != null) {
+            if (line.equals("")) { continue; }
+
+            String[] parts = line.split(",");
+            int index = Integer.parseInt(parts[0]);
+
+            for (String noun : parts[1].split(" ")) {
+                // check if noun is already in allNouns
+                if (this.allNouns.containsKey(noun)) {
+                    currentNounsList = this.allNouns.get(noun);
+                } else {
+                    currentNounsList = new ArrayList<>();
+                }
+
+                // check of index is already in allKeys
+                if (this.allKeys.containsKey(index)) {
+                    currentSynsetNouns = this.allKeys.get(index);
+                } else {
+                    currentSynsetNouns = new String();
+                }
+
+                currentNounsList.add(index);
+                currentSynsetNouns = parts[1];
+
+                this.allNouns.put(noun, currentNounsList);
+                this.allKeys.put(index, currentSynsetNouns);
+            }
+            this.V++;
+        }
+    }
+
+    private void loadHypernyms(String hypernyms) {
+        In in = new In(hypernyms);
+        String line;
+        ArrayList<Integer> edgeList;
+        while ((line = in.readLine()) != null) {
+            if (line.equals("")) continue;
+            String[] parts = line.split(",");
+            Integer index = Integer.parseInt(parts[0]);
+            // check if index exists
+            if (edges.get(index) != null) {
+                edgeList = edges.get(index);
+            } else {
+                edgeList = new ArrayList<>();
+            }
+
+            for (int i = 1; i < parts.length; i++) {
+                edgeList.add(Integer.parseInt(parts[i]));
+            }
+
+            edges.put(index, edgeList);
+        }
     }
 
     // do unit testing of this class
     public static void main(String[] args) {
         WordNet w = new WordNet("synsets.txt", "hypernyms.txt");
         System.out.println("power_plant is a noun? " + w.isNoun("power_plant"));
-        System.out.println("abracadabra is not? " + w.isNoun("abracadabra"));
+        System.out.println("abracadabra is too? " + w.isNoun("abracadabra"));
+        System.out.println("azkaba is not? " + w.isNoun("azkaba"));
 
     }
 }
